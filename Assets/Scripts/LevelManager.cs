a1994daa01e6b27aaab1c52caf1f297438895084
HansSima <SimaHonza@seznam.cz>
2022-01-28 13:13:17 +0100

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float waitToRespawn;
+
+    public static LevelManager instance;
+
+    public int gemsCollected;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespownPlayer()
+    {
+        StartCoroutine(RespawnCo());
+    }
+
+    private IEnumerator RespawnCo()
+    {
+        PlayerController.instance.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(waitToRespawn);
+
+        PlayerController.instance.gameObject.SetActive(true);
+
+        PlayerController.instance.transform.position = ChceckPointController.instance.spawnPoint;
+
+        PlayerHealthController.instance.currentHealth = PlayerHealthController.instance.maxHealth;
+        UIController.instance.UpdateHealthDisplay();
+    }
+
+}
---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth, maxHealth;
+
+    public float invincibleLenght;
+    private float invincibleCounter;
+
+    private SpriteRenderer theSR;
+
+    public GameObject deathEffect;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invincibleCounter > 0)
+        {
+            invincibleCounter -= Time.deltaTime;
+
+            if (invincibleCounter <= 0)
+            {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1);
+            }
+
+        }
+
+    }
+
+    public void DealDamage()
+    {
+        // currentHealth -= 1;
+        if (invincibleCounter <= 0) {
+            currentHealth--;
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+
+                //gameObject.SetActive(false);
+                Instantiate(deathEffect, transform.position, transform.rotation);
+                AudioManager.instance.PlaySFX(8);
+                LevelManager.instance.RespownPlayer();
+
+            }
+            else
+            {
+                invincibleCounter = invincibleLenght;
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 0.5f);
+
+                PlayerController.instance.KnockBack();
+            }
+
+        UIController.instance.UpdateHealthDisplay();
+
+        }
+
+    }
+
+    public void HealPlayer()
+    {
+        currentHealth++;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        UIController.instance.UpdateHealthDisplay();
+
+    }
+
+}
